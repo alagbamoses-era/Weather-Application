b17f5174b52421d02976e80a170afd6d27daaa87
Moses Agunbiade <agunbiademoses2009@gmail.com>
2023-05-22 13:03:39 +0100

    Add live API data

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp)
+    let hours = date.getHours()
+    if (hours < 10) {
+        `0${hours}`
+    }
+    let minutes = date.getMinutes()
+    if (minutes < 10) {
+        `0${minutes}`
+    }
+    let day = date.getDay()
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let dayWeek = days[day]
+    console.log(hours)
+
+
+
+
+    return `${dayWeek} ${hours}:${minutes}`
+
+
+}
+
 function displayTemperature(response){
-    console.log(response.data.main.temp)
+    console.log(response.data.dt)
+    let temperatureElement = document.querySelector("#temperature")
+    let humidityElement = document.querySelector("#humidity")
+    let windElement = document.querySelector("#wind")
+    let descriptionElement = document.querySelector("#description")
+    let dateElement = document.querySelector("#date")
+
+
+
+
+    temperatureElement.innerHTML= Math.round(response.data.main.temp)
+    humidityElement.innerHTML = Math.round(response.data.main.humidity)
+    windElement.innerHTML = Math.round(response.data.wind.speed)
+    descriptionElement.innerHTML = response.data.weather[0].description
+    dateElement.innerHTML = formatDate(response.data.dt *1000)
 }
 
 apiKey = "6ce66d083b4d6dddb74ba02266495c46"
