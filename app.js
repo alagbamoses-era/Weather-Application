91e59621fb98a86809c3c5ca2bd7ddbb2e422949
Moses Agunbiade <agunbiademoses2009@gmail.com>
2023-05-23 23:36:05 +0100

    Add if condition to Minutes and hours

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 function formatDate(timestamp){
     let date = new Date(timestamp)
     let hours = date.getHours()
-    if (hours < 10) {
+    if (hours < 10) { hours=
         `0${hours}`
     }
     let minutes = date.getMinutes()
-    if (minutes < 10) {
+    if (minutes < 10) { minutes =
         `0${minutes}`
     }
     let day = date.getDay()
@@ -23,12 +23,13 @@ function formatDate(timestamp){
 }
 
 function displayTemperature(response){
-    console.log(response.data.dt)
+    console.log(response.data)
     let temperatureElement = document.querySelector("#temperature")
     let humidityElement = document.querySelector("#humidity")
     let windElement = document.querySelector("#wind")
     let descriptionElement = document.querySelector("#description")
     let dateElement = document.querySelector("#date")
+    let iconElement = document.querySelector("#icon")
 
 
 
@@ -38,6 +39,8 @@ function displayTemperature(response){
     windElement.innerHTML = Math.round(response.data.wind.speed)
     descriptionElement.innerHTML = response.data.weather[0].description
     dateElement.innerHTML = formatDate(response.data.dt *1000)
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+    console.log(response.data.weather[0].icon)
 }
 
 apiKey = "6ce66d083b4d6dddb74ba02266495c46"
